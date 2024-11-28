e3bdf17fb17f468c32982ff6c9102ee1a54b2454
Lynn-Lu09 <luya0918@163.com>
2024-11-28 19:47:21 +0800

    Update fetch_cpi.py

--- scripts/fetch_cpi.py
+++ scripts/fetch_cpi.py
@@ -1,3 +1,5 @@
-from src.CPI_data_fetcher import fetch_cpi_data
-last_4_quarters = fetch_cpi_data()
+import sys
+sys.path.append('path/.../src')  # the path of "src" folder in your computer
+import CPI_data_fetcher 
+last_4_quarters = CPI_data_fetcher.fetch_cpi_data()
 print(last_4_quarters)
